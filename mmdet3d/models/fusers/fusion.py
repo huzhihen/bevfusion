3aa926de39bb9f1dc5c5065418176dae16f3e313
Zhiheng Hu <1852647560@qq.com>
2023-11-09 16:00:53 +0800

    Uni-BEVFusion-Final

--- mmdet3d/models/fusers/fusion.py
+++ mmdet3d/models/fusers/fusion.py
@@ -8,28 +8,91 @@ from mmdet3d.models.builder import FUSERS
 __all__ = ["LidarCameraFusion"]
 
 
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1      = nn.Conv2d(in_planes, in_planes // 16, 1, bias=False)
+        self.relu1    = nn.ReLU()
+        self.fc2      = nn.Conv2d(in_planes // 16, in_planes, 1, bias=False)
+        self.sigmoid  = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out     = avg_out + max_out
+        return self.sigmoid(out)
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv1   = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out    = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+
+class LidarCameraFusion_Block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(LidarCameraFusion_Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1   = nn.BatchNorm2d(out_channels)
+        self.relu  = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn2   = nn.BatchNorm2d(out_channels)
+        if stride != 1 or out_channels != in_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        else:
+            self.shortcut = None
+
+        self.ca = ChannelAttention(out_channels)
+        self.sa = SpatialAttention()
+
+    def forward(self, x):
+        residual = x
+        if self.shortcut is not None:
+            residual = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.ca(out) * out
+        out = self.sa(out) * out
+        out += residual
+        out = self.relu(out)
+        return out
+
+
 @FUSERS.register_module()
 class LidarCameraFusion(nn.Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(sum(in_channels), out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(True)
-        )
-        self.conv2 = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(out_channels, out_channels // 4, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(out_channels // 4),
-            nn.ReLU(True),
-            nn.Conv2d(out_channels // 4, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.Sigmoid()
-        )
+        self.fusion_conv = self._make_layer(block=LidarCameraFusion_Block,
+                                            input_channels=sum(in_channels),
+                                            output_channels=out_channels,
+                                            block_nums=2)
+
+    def _make_layer(self, block, input_channels, output_channels, block_nums=1):
+        layers = []
+        layers.append(block(input_channels, output_channels))
+        for i in range(block_nums - 1):
+            layers.append(block(output_channels, output_channels))
+        return nn.Sequential(*layers)
 
     def forward(self, inputs: List[torch.Tensor]) -> torch.Tensor:
-        x = self.conv1(torch.cat(inputs, dim=1))
-        x = self.conv2(x) * x
+        x = self.fusion_conv(torch.cat(inputs, dim=1))
         return x
